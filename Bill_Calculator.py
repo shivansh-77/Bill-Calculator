445b36d1fb675128333ca3d70974203feb8cdd7a
shivansh-77 <114559279+shivansh-77@users.noreply.github.com>
2024-04-29 11:46:51 +0530

    Add files via upload

---
+++ Bill_Calculator.py
@@ -0,0 +1,7 @@
+print("Welcome to the Tip Calculator")
+Total_Bill = float(input("What was the total Bill ?"))
+Tip = input("How much would you like to give? 10 , 12 , 15")
+number = input("How many people to split the bill?")
+Total_Tip = (15/100) * float(Total_Bill)
+T_B = Total_Tip + Total_Bill
+print(T_B/5)5
